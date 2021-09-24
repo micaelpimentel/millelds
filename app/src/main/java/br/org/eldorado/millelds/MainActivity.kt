45b46f3fa978445a7399f17a627c3664f4ccb5b6
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-24 10:50:33 -0400

    Test with button ripple effect

--- app/src/main/java/br/org/eldorado/millelds/MainActivity.kt
+++ app/src/main/java/br/org/eldorado/millelds/MainActivity.kt
@@ -2,10 +2,21 @@ package br.org.eldorado.millelds
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.org.eldorado.millelds.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        binding.button.
     }
 }

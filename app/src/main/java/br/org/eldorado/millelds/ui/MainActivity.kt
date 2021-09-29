8473799c7e51bfeb4d52275c8b0a58ed855fae5f
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-29 18:10:17 -0400

    Added splash screen

--- app/src/main/java/br/org/eldorado/millelds/ui/MainActivity.kt
+++ app/src/main/java/br/org/eldorado/millelds/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package br.org.eldorado.millelds
+package br.org.eldorado.millelds.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/br/org/eldorado/millelds/ui/SplashActivity.kt
@@ -0,0 +1,21 @@
+package br.org.eldorado.millelds.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.org.eldorado.millelds.R
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}

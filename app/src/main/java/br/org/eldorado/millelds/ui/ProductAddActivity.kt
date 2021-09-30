a4d6fa8293cb5cb1458f9b8b39ad3d863b0c6852
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-30 18:32:05 -0400

    Added product add form

---
+++ app/src/main/java/br/org/eldorado/millelds/ui/ProductAddActivity.kt
@@ -0,0 +1,43 @@
+package br.org.eldorado.millelds.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import br.org.eldorado.millelds.dao.ProductDAO
+import br.org.eldorado.millelds.databinding.ActivityProductAddBinding
+import br.org.eldorado.millelds.model.Product
+import com.google.android.material.snackbar.Snackbar
+
+class ProductAddActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityProductAddBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setupButton()
+    }
+
+    private fun setupButton() {
+        with(binding) {
+            registerProductButton.setOnClickListener {
+                val name = "${productNameTextField.editText?.text}"
+                val description = "${productDescriptionTextField.editText?.text}"
+                val price = "${productPriceTextView.editText?.text}"
+
+                Product(
+                    name = name,
+                    description = description,
+                    price = price.toBigDecimal()
+                ).let {
+                    Log.d("MICAEL", "$it")
+                    ProductDAO().add(it)
+                }
+                Log.d("MICAEL", ProductDAO().getAll().toString())
+
+                finish()
+            }
+        }
+    }
+}

e6e6645f112e4d3b987f60c1042023cb69992395
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-10-01 16:15:30 -0400

    Implemented update recyclerview

--- app/src/main/java/br/org/eldorado/millelds/ui/MainActivity.kt
+++ app/src/main/java/br/org/eldorado/millelds/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package br.org.eldorado.millelds.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import br.org.eldorado.millelds.R
 import br.org.eldorado.millelds.dao.ProductDAO
 import br.org.eldorado.millelds.databinding.ActivityMainBinding
+import br.org.eldorado.millelds.model.Product
 import br.org.eldorado.millelds.ui.adapter.ProductListAdapter
 import com.google.android.material.snackbar.Snackbar
 
@@ -14,17 +16,27 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val productList = ProductDAO().getAll().toMutableList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         setupViews()
     }
 
+    override fun onResume() {
+        super.onResume()
+        updateProductList()
+    }
+
+    private fun updateProductList() {
+        productList.clear()
+        productList.addAll(ProductDAO().getAll().toMutableList())
+        binding.recyclerView.adapter?.notifyItemInserted(productList.lastIndex)
+    }
+
     private fun setupViews() {
-
-        binding.recyclerView.adapter = ProductListAdapter(ProductDAO().getAll())
-
+        binding.recyclerView.adapter = ProductListAdapter(productList)
         binding.button.setOnClickListener {
             startActivity(Intent(this,ProductAddActivity::class.java))
         }

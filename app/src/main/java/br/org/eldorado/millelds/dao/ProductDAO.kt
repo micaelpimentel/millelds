c83b176c56ba054a37565d2024d635bb755243b2
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-10-11 14:53:08 -0400

    Fixed product image width

--- app/src/main/java/br/org/eldorado/millelds/dao/ProductDAO.kt
+++ app/src/main/java/br/org/eldorado/millelds/dao/ProductDAO.kt
@@ -7,7 +7,7 @@ class ProductDAO {
         private val products = mutableListOf(
             Product(
                 name = "Cesta de frutas 1",
-                description = "Cesta básica padrão",
+                description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non sodales neque. Pulvinar elementum integer enim neque volutpat ac. Vitae tempus quam pellentesque nec nam. Habitant morbi tristique senectus et netus et malesuada. Semper quis lectus nulla at. Sed enim ut sem viverra. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Rhoncus urna neque viverra justo nec ultrices dui. Amet massa vitae tortor condimentum lacinia quis vel eros donec. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Eget velit aliquet sagittis id consectetur. Neque vitae tempus quam pellentesque nec nam aliquam. Scelerisque fermentum dui faucibus in ornare quam viverra. Pellentesque dignissim enim sit amet venenatis urna cursus eget nunc. Mattis nunc sed blandit libero volutpat sed cras ornare arcu. Est ante in nibh mauris. Morbi enim nunc faucibus a. Ut faucibus pulvinar elementum integer enim",
                 price = "19.00".toBigDecimal()
             ),
             Product(

68ddca6eaae26b0bb4bf84c07f0224f79ca68296
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-10-11 12:42:45 -0400

    Added placeholder and error to image load

--- app/src/main/java/br/org/eldorado/millelds/extensions/ImageViewExtensions.kt
+++ app/src/main/java/br/org/eldorado/millelds/extensions/ImageViewExtensions.kt
@@ -1,8 +1,13 @@
 package br.org.eldorado.millelds.extensions
 
 import android.widget.ImageView
+import br.org.eldorado.millelds.R
 import coil.load
 
 fun ImageView.tryLoadImage(url: String? = null) {
-    load(url)
+    load(url) {
+        fallback(R.drawable.image_error)
+        error(R.drawable.image_error)
+        placeholder(R.drawable.placeholder)
+    }
 }
